c7a26e4a7927aaff3a703ea2cb03b6933023242c
hao <1394168335@qq.com>
2018-12-30 12:22:46 +0800

    first commit

---
+++ docker-entrypoint.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+## config param
+endpoint=$ENDPOINT
+namespace=$NAMESPACE 
+accessKey=$ACCESS_KEY 
+secretKey=$SECRET_KEY 
+dataId=$DATA_ID
+group=$GROUP
+## config param end
+
+## get serverIp from address server
+serverIp=`curl $endpoint:8080/diamond-server/diamond -s | awk '{a[NR]=$0}END{srand();i=int(rand()*NR+1);print a[i]}'`
+
+## config sign
+timestamp=`(date +%s%N)/1000000`
+signStr=$namespace+$group+$timestamp
+signContent=`echo -n $signStr | openssl dgst -hmac $secretKey -sha1 -binary | base64`
+
+## request
+TEXT=$(curl -s -H "Spas-AccessKey:"$accessKey -H "timeStamp:"$timestamp -H "Spas-Signature:"$signContent "http://"$serverIp":8080/diamond-server/config.co?dataId="$dataId"&group="$group"&tenant="$namespace)
+echo $TEXT > /etc/nginx/conf.d/default.conf
+exec nginx
