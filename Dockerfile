48e1e522452685391b441092017f921ba6e32d46
hao <1394168335@qq.com>
2020-03-26 18:28:53 +0800

    fix

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,4 @@
 FROM nginx:alpine
-COPY nginx.conf /etc/nginx/nginx.conf
 COPY docker-entrypoint.sh /docker-entrypoint.sh
 RUN apk add --no-cache bash curl openssl coreutils && chmod +x /docker-entrypoint.sh
 STOPSIGNAL SIGTERM
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,14 +1,26 @@
-#!/bin/sh
-CONFIG="upstream local-proxy {\n"
+#!/bin/bash
+CONFIG="user nginx;
+worker_processes auto;
+
+worker_rlimit_nofile 65536;
+error_log  /var/log/nginx/error.log warn;
+pid        /var/run/nginx.pid;
+
+events {
+    worker_connections 4096;
+    multi_accept on;
+    use epoll;
+}
+stream {upstream local-proxy {"
 for (( i=0; i>-1; i++ ))
 do
 	upstream=`eval echo '$'"UPSTREAM_${i}"`
 	if [ -z ${upstream} ];then
 		break
 	fi
-	CONFIG=${CONFIG}"\tserver "${upstream}";\n"
+	CONFIG=${CONFIG}"server "${upstream}";"
 done
 
-CONFIG=${CONFIG}"}\nserver{\n\tlisten 1080;\n\tproxy_pass local-proxy;\n}"
-echo ${CONFIG}> /etc/nginx/conf.d/default.conf
+CONFIG=${CONFIG}"}server{listen 1080;proxy_pass local-proxy;}}"
+echo ${CONFIG}> /etc/nginx/nginx.conf
 exec nginx -g 'daemon off;'
