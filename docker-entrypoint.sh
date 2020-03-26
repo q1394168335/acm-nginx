16b320df213a9f858e157ecbf444f716d6fb2bc3
hao <1394168335@qq.com>
2020-03-26 17:49:04 +0800

    add

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,23 +1,14 @@
-#!/bin/bash
+#!/bin/sh
+CONFIG="upstream local-proxy {\n"
+for (( i=0; i>-1; i++ ))
+do
+	upstream=`eval echo '$'"UPSTREAM_${i}"`
+	if [ -z ${upstream} ];then
+		break
+	fi
+	CONFIG=${CONFIG}"\tserver "${upstream}";\n"
+done
 
-## config param
-endpoint=$ENDPOINT
-namespace=$NAMESPACE 
-accessKey=$ACCESS_KEY 
-secretKey=$SECRET_KEY 
-dataId=$DATA_ID
-group=$GROUP
-## config param end
-
-## get serverIp from address server
-serverIp=`curl $endpoint:8080/diamond-server/diamond -s | awk '{a[NR]=$0}END{srand();i=int(rand()*NR+1);print a[i]}'`
-
-## config sign
-timestamp=`echo $[$(date +%s%N)/1000000]`
-signStr=$namespace+$group+$timestamp
-signContent=`echo -n $signStr | openssl dgst -hmac $secretKey -sha1 -binary | base64`
-
-## request
-TEXT=$(curl -s -H "Spas-AccessKey:"$accessKey -H "timeStamp:"$timestamp -H "Spas-Signature:"$signContent "http://"$serverIp":8080/diamond-server/config.co?dataId="$dataId"&group="$group"&tenant="$namespace)
-echo $TEXT > /etc/nginx/conf.d/default.conf
+CONFIG=${CONFIG}"}\nserver{\n\tlisten 1080;\n\tproxy_pass local-proxy;\n}"
+echo ${CONFIG}> /etc/nginx/conf.d/default.conf
 exec nginx -g 'daemon off;'
